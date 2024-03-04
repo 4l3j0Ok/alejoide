599009eead85e586472d6fbba73f72811301a223
4l3j0Ok <alejofsarmiento@gmail.com>
2024-03-04 16:41:34 -0300

    CORS multiple allowed origins from envvar

--- src/rxconfig.py
+++ src/rxconfig.py
@@ -4,5 +4,5 @@ import reflex as rx
 
 config = rx.Config(
     app_name="alejoide",
-    cors_allowed_origins=[os.getenv("APP_URL", "http://localhost:3000")]
+    cors_allowed_origins=os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:3000").split(",")
 )
