02e9beb2ae83d5cf47e67991a9131cbe4ad5c378
4l3j0Ok <alejofsarmiento@gmail.com>
2024-04-16 23:15:30 -0300

    changed my age

--- src/alejoide/modules/constants.py
+++ src/alejoide/modules/constants.py
@@ -2,7 +2,6 @@ import os
 from enum import Enum
 
 
-
 class App(Enum):
     NAME = "Alejoide"
     VERSION = "v1"
@@ -41,7 +40,7 @@ gtag('config', '{TAG}');
 
 class AboutMe(Enum):
     DESCRIPTION = [
-        "Mi nombre es Alejo. Tengo 22 años y vivo en Argentina.",
+        "Mi nombre es Alejo. Tengo 23 años y vivo en Argentina.",
         "En 2013 decidí orientar mis conocimientos hacia el mundo de la programación y la informática, estudiando informática en mi escuela secundaria técnica.",
         "En 2020, al finalizar mis estudios secundarios, comencé a trabajar como desarrollador backend en una empresa de desarrollo de software, donde aprendí a trabajar con Python, Flask, FastAPI, entre otras tecnologías.",
         "A comienzos de 2022, se me ofreció la oportunidad de trabajar como SRE/DevOps, un mundo nuevo con nuevos desafíos que me tocó enfrentar.",
@@ -53,31 +52,31 @@ class AboutMe(Enum):
             "title": "SRE / DevOps",
             "description": [
                 "Actualmente me encuentro trabajando de SRE/DevOps, donde trabajo principalmente con tecnologías como Docker, Apache, Jenkins, GitLab, LXC, NGINX, que me permiten crear infraestructura y pipelines para aplicar prácticas de CI/CD.",
-                "Además de eso, actualmente estoy aprendiendo otras tecnologías como Kubernetes y plataformas Cloud."
-            ]
+                "Además de eso, actualmente estoy aprendiendo otras tecnologías como Kubernetes y plataformas Cloud.",
+            ],
         },
         {
             "icons": "fa-brands:python",
             "title": "Backend",
             "description": [
                 "Tengo experiencia trabajando como desarrollador backend, siendo mi fuerte principal el desarrollo en Python, lenguaje que me apasiona y con el que he trabajado tanto en proyectos personales como profesionales con frameworks como Flask, FastAPI, PyQT, Reflex, entre otros."
-            ]
+            ],
         },
         {
             "icons": "nonicons:react-16",
             "title": "Frontend",
             "description": [
                 "Me encuentro incursionando en el mundo del desarrollo de frontend, donde estoy aprendiendo Reflex, React y otras tecnologías con el objetivo de poder crear aplicaciones web y móviles.",
-                "De hecho, ¡este sitio web está desarrollado por mí utilizando Reflex!"
-            ]
+                "De hecho, ¡este sitio web está desarrollado por mí utilizando Reflex!",
+            ],
         },
         {
             "icons": "ic:baseline-school",
             "title": "Profesor",
             "description": [
                 "Me gusta mucho enseñar, por lo que también estoy apuntando a ser profesor de programación en escuelas de nivel superior."
-            ]
-        }
+            ],
+        },
     ]
 
 
@@ -85,7 +84,7 @@ class Projects(Enum):
     DESCRIPTION = [
         "Estos son algunos de los proyectos personales en los que he trabajado, tanto en mi tiempo libre.",
         "Tengo muchos más proyectos además de estos, pero los que se muestran aquí son los que considero más importantes.",
-        "¡También podés ver todos mis proyectos en mi GitHub!"
+        "¡También podés ver todos mis proyectos en mi GitHub!",
     ]
     CARDS = [
         {
@@ -107,7 +106,7 @@ class Projects(Enum):
             "image": "projects/bcra_scraper_api.png",
             "repo_url": "https://github.com/4l3j0Ok/bcra-scraper-api",
             "app_url": "https://bcra-scraper-api.alejoide.com",
-            "is_disabled": False
+            "is_disabled": False,
         },
         {
             "icons": "fa-brands:python|heroicons:window-20-solid",
@@ -117,7 +116,7 @@ class Projects(Enum):
             ],
             "image": "projects/fg_anti_sniper.png",
             "repo_url": "https://github.com/4l3j0Ok/fall-guys-anti-sniper",
-            "is_disabled": False
+            "is_disabled": False,
         },
         {
             "icons": "fa-brands:docker",
@@ -127,44 +126,22 @@ class Projects(Enum):
             ],
             "image": "projects/prodesk_stacks.png",
             "repo_url": "https://github.com/4l3j0Ok/prodesk-stacks",
-            "is_disabled": False
-        }
+            "is_disabled": False,
+        },
     ]
 
-    GITHUB_BUTTON = {
-        "icon": "fa-brands:github",
-        "text": "Ver en GitHub"
-        }
+    GITHUB_BUTTON = {"icon": "fa-brands:github", "text": "Ver en GitHub"}
 
-    APP_BUTTON = {
-        "icon": "fa-solid:link",
-        "text": "Ver app"
-    }
+    APP_BUTTON = {"icon": "fa-solid:link", "text": "Ver app"}
 
 
 class Footer(Enum):
     ICONS = [
-        {
-            "icon": "fa-brands:instagram",
-            "link": "https://instagram.com/4l3j0"
-        },
-        {
-            "icon": "fa-brands:twitter",
-            "link": "https://twitter.com/alejoide_"
-        },
-        {
-            "icon": "fa-brands:github",
-            "link": "https://github.com/4l3j0Ok"
-        },
-        {
-            "icon": "fa-brands:linkedin",
-            "link": "https://www.linkedin.com/in/alejoide"
-        },
-        {
-            "icon": "tabler:mail-filled",
-            "link": Links.EMAIL.value,
-            "new_tab": False
-        },
+        {"icon": "fa-brands:instagram", "link": "https://instagram.com/4l3j0"},
+        {"icon": "fa-brands:twitter", "link": "https://twitter.com/alejoide_"},
+        {"icon": "fa-brands:github", "link": "https://github.com/4l3j0Ok"},
+        {"icon": "fa-brands:linkedin", "link": "https://www.linkedin.com/in/alejoide"},
+        {"icon": "tabler:mail-filled", "link": Links.EMAIL.value, "new_tab": False},
         # {
         #     "icon": "fa-solid:ellipsis-h",
         #     "link": "https://links.alejoide.com"
